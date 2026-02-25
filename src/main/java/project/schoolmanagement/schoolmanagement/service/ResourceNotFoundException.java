f6003ca25162516c8704574fce1c6677a970d9dd
Đỗ An Bình <binhdath06811@gmail.com>
2026-02-25 14:54:55 +0700

    delete unnecessary file

--- src/main/java/project/schoolmanagement/schoolmanagement/service/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package project.schoolmanagement.schoolmanagement.service;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
